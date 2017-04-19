e773e09be2fb7703cf819a6ed2014e0f098c5023
Debbie Milburn <debbiemilburn2@gmail.com>
2017-04-19 09:42:14 -0400

    Create new functionality

--- app/controllers/bumblebees_controller.rb
+++ app/controllers/bumblebees_controller.rb
@@ -2,3 +2,12 @@ get '/bumblebees' do
   @bumblebees = Bumblebee.all
   erb :'/bumblebees/index'
 end
+
+get '/bumblebees/new' do
+  erb :'/bumblebees/new'
+end
+
+post '/bumblebees' do
+  Bumblebee.create(name: params[:name], job: params[:job])
+  redirect '/bumblebees'
+end
